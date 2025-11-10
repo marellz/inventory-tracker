9439b8f6aae512f5986580726d84faa442a7db39
Dave Njoroge <davenjoroge10@gmail.com>
2025-11-10 23:59:38 +0300

    api: fix: enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.WEB_URL ?? 'http://inventrak.localhost',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
